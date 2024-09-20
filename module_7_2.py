12ea51f6faaa438904f378ac76578e92ff691453
GrayThing <samorodov-artem@mail.ru>
2024-09-20 11:56:13 +0300

    Выполнено задание: 2023/11/16 00:00|Домашнее задание по теме "Позиционирование в файле".

---
+++ module_7_2.py
@@ -0,0 +1,26 @@
+# coding: utf8
+
+def custom_write(file_name, strings: list):
+    strings_positions = {}
+    file = open(file_name, 'w', encoding='utf-8')
+    string_number = 1
+    for string in strings:
+        string_beginning = file.tell()
+        file.write(f'{string}\n')
+        strings_positions.update({f'{(string_number, string_beginning)}': string})
+        string_number += 1
+    file.close()
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
+
